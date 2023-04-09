1a99762efa73767bd16a9c5e3700418057a3c0f9
@david.carvalho <david.carvalho@valecard.com.br>
2023-04-09 15:15:49 -0300

    Aula02.

--- src/GeradouraDeFigurinhas.java
+++ src/GeradouraDeFigurinhas.java
@@ -11,7 +11,7 @@ public class GeradouraDeFigurinhas {
 
     public void cria(InputStream inputStream, String nomeArquivo) throws Exception {
 
-        // Leitura da imagem
+        // Leitura da imagem  (O Polimorfismo é um mecanismo por meio do qual selecionamos as funcionalidades utilizadas de forma dinâmica por um programa no decorrer de sua execução.)
         // BufferedImage imagemOrigem = ImageIO.read(new File("entrada/TopMovies_1.jpg"));
         // InputStream inputStream = new FileInputStream(new File("entrada/TopMovies_1.jpg"));
         // BufferedImage imagemOrigem = ImageIO.read(inputStream);
@@ -19,7 +19,6 @@ public class GeradouraDeFigurinhas {
         // URL("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies_1.jpg")
         // .openStream();
         BufferedImage imagemOrigem = ImageIO.read(inputStream);
-
 
         // Criar nova imagem, com transparencia, tamanho novo
         int largura = imagemOrigem.getWidth();
@@ -31,7 +30,7 @@ public class GeradouraDeFigurinhas {
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOrigem, 0,0, null );
 
-         // Configurar a fonte e cor
+         // Configurar a fonte e cor (Isso é setar a fonte)
          var fonte = new Font(Font.SANS_SERIF,Font.BOLD, 80);
         graphics.setFont(fonte);
         graphics.setColor(Color.ORANGE);
@@ -41,8 +40,8 @@ public class GeradouraDeFigurinhas {
         graphics.drawString("das", 50 ,novaAltura -50 );
         graphics.drawString("Galaxias", 230 ,novaAltura -20);
        
-
         // Escrever  a nova imagem  em  um arquivo
+        // ImageIO.write(novaImagem, formatName: "png", new File("saida/figurinha.png"));
         ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
     }
